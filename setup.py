7bbd5fffe342e01fcecb4046c5ffdbac1f4e9d3b
Yaseen <43935210+Yaseen549@users.noreply.github.com>
2021-03-24 00:52:23 +0530

    Add files via upload

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ setup(
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A Startup Library still in progress',   # Give a short description about your library
   long_description="Am a long description", #try
-  long_description=long_description,
   long_description_content_type="text/markdown",        # Long description
   author = 'MOHAMED YASEEN SHERIFF S',                   # Type in your name
   author_email = 'fantasticyaseenshariff@gmail.com',      # Type in your E-Mail

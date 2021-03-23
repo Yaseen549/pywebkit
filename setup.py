a06561622c6153d35801ede1241d915a0f170afb
Yaseen <43935210+Yaseen549@users.noreply.github.com>
2021-03-24 00:37:49 +0530

    Add files via upload

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
   version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A Startup Library still in progress',   # Give a short description about your library
+  long_description_content_type="text/markdown",        # Long description
   author = 'MOHAMED YASEEN SHERIFF S',                   # Type in your name
   author_email = 'fantasticyaseenshariff@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/Yaseen549/pywebkit',   # Provide either the link to your github or to your website

67bd38e93150fcf3ebb9318b94c017d53a4e8963
Yaseen <43935210+Yaseen549@users.noreply.github.com>
2021-03-28 22:05:44 +0530

    upload mathlib.py

--- pywebkit/__init__.py
+++ pywebkit/__init__.py
@@ -1,2 +1,6 @@
+# CSS Class Maker
 from pywebkit.css_class_maker import css_class_generator
 from pywebkit.css_class_maker import *
+
+# Mathlib
+from pywebkit.mathlib import *
---
+++ pywebkit/mathlib.py
@@ -0,0 +1,37 @@
+# Simple Math Libraries
+
+def add(*nums):
+    number = 0
+    for num in nums:
+        number += num
+    return number
+
+
+def sub(*nums):
+    number = 0
+    for num in nums:
+        number -= num
+    return number
+
+
+def multiply(*nums):
+    number = 1
+    for num in nums:
+        number *= num
+    return number
+
+
+def sqrt(num):
+	return num**2
+
+
+def cube(num):
+	return num**3
+
+
+def powerof(num1, num2):
+	return num1**num2
+
+
+def div2num(dividend, divisor):
+	return dividend/divisor

fbe8b46d628509ecc0e24279c419bbd9eb3626ca
Yaseen <43935210+Yaseen549@users.noreply.github.com>
2021-03-28 15:50:52 +0530

    Newlines refactored

--- pywebkit/css_class_maker.py
+++ pywebkit/css_class_maker.py
@@ -1,13 +1,13 @@
 # Css Class Generator in python
 
 def css_class_generator():
-    filename = input("File name: ")
-    total_num_of_classes = int(input("How many CSS classes do you need?: "))
+    filename = input("File Name: ")
+    total_num_of_classes = int(input("How many CSS class you need? "))
     myfile = open( filename+".css", "w+")
 
     for i in range(int(total_num_of_classes)):
         print("Class ", int(i+1))
-        total_num_of_pv = int(input("How many properties?: "))
+        total_num_of_pv = int(input("How many properties? "))
         css_selector = input("Selector: ")
         line1 = "." + css_selector +"{\n"
         myfile.writelines(line1)
@@ -21,3 +21,7 @@ def css_class_generator():
         line3 = "}\n\n"
         myfile.writelines(line3)
     myfile.close()
+
+# New Lines
+for f in range(12):
+    exec(f"def newline{f+1}(): print('\\n'*{f+1})")

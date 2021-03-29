4707526e2b558792ca60337d28d7787f27cda36b
Temerold <67090612+Temerold@users.noreply.github.com>
2021-03-29 08:57:44 +0200

    ... Minor formatting

--- pywebkit/css_class_maker.py
+++ pywebkit/css_class_maker.py
@@ -5,8 +5,8 @@ def css_class_generator():
     total_num_of_classes = int(input("How many CSS classes do you need?: "))
     myfile = open( filename + ".css", "w+")
 
-    for i in range(int(total_num_of_classes)):
-        print(f"Class {i+1}:")
+    for i in range(total_num_of_classes):
+        print(f"Class {i}:")
         total_num_of_pv = int(input("How many properties?: "))
         css_selector = input("Selector: ")
         line1 = "." + css_selector +"{\n"
@@ -22,6 +22,6 @@ def css_class_generator():
         myfile.writelines(line3)
     myfile.close()
 
-# New Lines
+# Newlines
 for f in range(12):
     exec(f"def newline{f+1}(): print('\\n'*{f+1})")
